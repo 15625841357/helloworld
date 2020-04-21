45055642ec51c96b7c4b86befb7bb9fa4e9943b7
monkeyqi <qimonkey@outlook.com>
2020-04-21 15:25:05 +0800

    test

--- src/MscInvisibleDemo/mtest.java
+++ src/MscInvisibleDemo/mtest.java
@@ -11,6 +11,8 @@ public class mtest {
         Thread t = new Thread(() -> {
             MscTest.main(new String[0]);
             System.out.println("子程序运行中");
+            System.out.println();
+            System.out.println("吴俊淇");
         });
         t.start();
         t.join();
